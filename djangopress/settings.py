8d54c9671b69fffdad15eb16369bf9ddf4829659
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-01-12 16:42:50 -0300

    feat: update blog app configuration and add initial migration files

--- djangopress/settings.py
+++ djangopress/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "djangopress.apps.blog.apps.BlogConfig"
 ]
 
 MIDDLEWARE = [
