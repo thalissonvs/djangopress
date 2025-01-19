01f71a6ff0977c155f84d09090c0028676e4afa2
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-01-18 21:08:11 -0300

    refactor: refactor post list view to use Django's class-based views

--- djangopress/apps/blog/views.py
+++ djangopress/apps/blog/views.py
@@ -1,25 +1,14 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page', 1)
-    
-    try:
-        posts = paginator.get_page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.get_page(1)
-    except EmptyPage:
-        posts = paginator.get_page(paginator.num_pages)
-    
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_detail(request, year, month, day, post):
     blog_post = get_object_or_404(
